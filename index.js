00e1aac383a1e3c39aa92f287c7f1c73a2eada08
Julia Frances <julia.horan28@gmail.com>
2022-02-05 23:11:09 -0500

    praticing functions

---
+++ index.js
@@ -0,0 +1,14 @@
+var n = 5;
+var a = [4, 0, 1, -2, 3];
+var results = [];
+
+function solution(n, a) {
+  for (var i = 0; i < a.length; i++) {}
+
+  console.log(n + a[i]);
+  // output
+  // [4,5, -1, 2,3]
+
+  // var num = Math.floor(Math.random()* combined.length);
+  // results+= combined[num]
+}
